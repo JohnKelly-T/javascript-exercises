9a0840408bfd04457a9bebd14d1fd856b47413dd
john-kelly <johnkellytemena@gmail.com>
2024-10-20 17:01:19 +0800

    Add functionality to reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    wordArray = word.split("");
+    n = wordArray.length;
 
+    for (let i = 0; i < n/2 ; i++) {
+        [wordArray[i], wordArray[n - (i+1)]] = [wordArray[n - (i+1)], wordArray[i]]
+    }
+
+    return wordArray.join("");
 };
 
 // Do not edit below this line
