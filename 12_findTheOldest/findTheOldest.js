b56c22774a30ad76ad7da8c3577866116cfeb159
john-kelly <johnkellytemena@gmail.com>
2024-11-14 17:03:50 +0800

    Complete the function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let sortedAge = array.toSorted((a, b) => {
+        let aAge;
+        let bAge;
 
+        aAge = !(a.yearOfDeath) ? (new Date()).getFullYear() - a.yearOfBirth : a.yearOfDeath - a.yearOfBirth;
+        bAge = !(b.yearOfDeath) ? (new Date()).getFullYear() - b.yearOfBirth : b.yearOfDeath - b.yearOfBirth;
+
+        return aAge - bAge;
+    });
+
+    return sortedAge.at(-1);
 };
 
 // Do not edit below this line
