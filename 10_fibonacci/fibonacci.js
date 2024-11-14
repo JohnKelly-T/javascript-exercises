3cbe602548be5477d486093b262263b6f597bb90
john-kelly <johnkellytemena@gmail.com>
2024-11-14 16:34:22 +0800

    Add functionality to function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n == 0) return 0;
+    if (n < 0) return "OOPS";
 
+    let firstNum = 1;
+    let sum = 1;
+
+    for (let i = 2; i < n; i++) {
+        [firstNum, sum] = [sum, firstNum + sum];
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
