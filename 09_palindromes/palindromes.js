125b6964c84d0dfef6c5d8f6578b30a6a62e51d7
john-kelly <johnkellytemena@gmail.com>
2024-11-14 16:16:36 +0800

    Add functionality to function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    // remove period at end of phrase
+    let newPhrase;
+    let isPalindrome = true;
+    if ([".", "?", "!"].includes(phrase.slice(-1))) {
+        newPhrase = phrase.slice(0, -1);
+    } else {
+        newPhrase = phrase;
+    }
 
+    // remove commas and spaces
+    newPhrase = newPhrase.split(", ").join("");
+    newPhrase = newPhrase.split(" ").join("");
+    newPhrase = newPhrase.toLowerCase();
+
+    for (let i = 0; i < Math.floor(newPhrase.length / 2); i++) {
+        if (!(newPhrase[i] === newPhrase[newPhrase.length - (i + 1)])) {
+            isPalindrome = false;
+        }
+    }
+
+    return isPalindrome;
 };
 
 // Do not edit below this line
